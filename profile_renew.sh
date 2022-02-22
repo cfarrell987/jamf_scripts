7568cc96c89eec5045f4b83697137a6fcfb856fe
Caleb Farrell <caleb.farrell@introhive.com>
2022-02-22 13:34:33 -0400

    Modified the echo to inform the user to click the notification and follow on-screen prompts

--- profile_renew.sh
+++ profile_renew.sh
@@ -5,14 +5,23 @@
 # Description: When the Jamf MDM profile doesn't apply correctly or becomes corrupted
 # This script can be sent to the user and ran to renew the profile and reboot
 
-if ! [ $(id -u) = 0 ]; then
-	echo "Please run this script with sudo."
-	exit 1
-else
-	profiles renew -type enrollment -verbose > ~/profile_renew.log
-	sleep 2
-	echo "Your Mac will restart in 5 minutes. Please save any work."
-	sleep 2
-	shutdown -r +5
-	sleep 2
+
+
+if (($EUID != 0)); then
+  if [[ -t 1 ]]; then
+    sudo "$0" "$@"
+  else
+    exec 1>output_file
+    gksu "$0 $@"
+  fi
+  exit
 fi
+
+sudo profiles renew -type enrollment -verbose > ~/profile_renew.log
+sleep 2
+echo "Please click on the Notification that has appeared in the top right. Follow the prompts on screen."
+sleep 5
+echo "Your Mac will restart in 5 minutes. Please save any work."
+sleep 2
+#shutdown -r +5
+sleep 2
