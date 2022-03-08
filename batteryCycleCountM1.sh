f30781e32441ea37e7c5211f2496717677bc75fd
Caleb Farrell <caleb@Calebs-MacBook-Pro.local>
2022-03-08 09:54:24 -0400

    Changed: - Added command to ensure the check is run as root.

--- batteryCycleCountM1.sh
+++ batteryCycleCountM1.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-# Name: logCollector.sh
+# Name: batteryCycleCountM1.sh
 # Created on: Febuary 4, 2021
 # Created by: Caleb Farrell<caleb.farrell@introhive.com>
 # Description: Since Apple Silicon change the system profiler output,
 # The Extention attribute to collect this doesn't work anymore. This is an updated version
+
+[ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
+
  echo "<result>$(system_profiler SPPowerDataType | grep "Cycle Count" | awk '{print $3}')</result>"
